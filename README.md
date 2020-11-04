894089de8bbcb62e814c78483fa4d3dfb97d0c8c
Tanner Embry <awesomete@gmail.com>
2020-11-03 19:59:14 -0600

    cleanup old code and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SpigotPlugin
-Wireframe base spigot plugin
+
+Use this as the base project to create a Minecraft Spigot plugin. All dependencies are handled through maven. To build the plugin, simply run:
+
+```js
+mvn package
+```
+This will create a jar file that you can place in the *plugins* folder of your Minecraft server.
+
+For plugin development help, please check out the forums here: https://www.spigotmc.org/forums/spigot-plugin-development.52/
--- src/main/java/com/snowgears/spigotplugin/listener/DefaultListener.java
+++ src/main/java/com/snowgears/spigotplugin/listener/DefaultListener.java
@@ -3,21 +3,18 @@ package com.snowgears.spigotplugin.listener;
 import com.snowgears.spigotplugin.SpigotPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryInteractEvent;
-
-import java.util.HashMap;
+import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class DefaultListener implements Listener {
 
     private SpigotPlugin plugin = SpigotPlugin.getPlugin();
-    private HashMap<String, Integer> shopBuildLimits = new HashMap<String, Integer>();
 
     public DefaultListener(SpigotPlugin instance) {
         plugin = instance;
     }
 
     @EventHandler
-    public void onPlayerJoin(InventoryInteractEvent event){
+    public void onPlayerJoin(InventoryClickEvent event){
        System.out.println("[SpigotPlugin] InventoryInteractEvent fired.");
     }
 }
